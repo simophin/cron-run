18fa3a62252d8e8e4d5cd33012e4b92e43dfe151
fanchao <git@fanchao.dev>
2023-10-23 21:18:22 +1100

    Install script

--- install.sh
+++ install.sh
@@ -11,16 +11,24 @@ fi
 # Detect CPU arch
 ARCH=$(uname -m)
 
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+LATEST_VERSION="`get_latest_release simophin/cron-run`"
+
 # Download latest version of the binary from Github
 echo "Downloading latest version of cron-run for $ARCH..."
 
 if [[ -w /usr/local/bin ]]; then
     echo "Installing to /usr/local/bin..."
-    curl -fL "https://github.com/simophin/cron-run/releases/download/latest/cron-run.$ARCH" -o /usr/local/bin/cron-run
+    curl -fL "https://github.com/simophin/cron-run/releases/download/$LATEST_VERSION/cron-run.$ARCH" -o /usr/local/bin/cron-run
     chmod +x /usr/local/bin/cron-run
 else
     echo "Installing to /usr/local/bin (requires sudo)..."
-    sudo "curl -fL "https://github.com/simophin/cron-run/releases/download/latest/cron-run.$ARCH" -o /usr/local/bin/cron-run && \
+    sudo "curl -fL "https://github.com/simophin/cron-run/releases/download/$LATEST_VERSION/cron-run.$ARCH" -o /usr/local/bin/cron-run && \
             chmod +x /usr/local/bin/cron-run"
 fi
 
