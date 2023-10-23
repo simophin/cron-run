c6c1882d308c232c65ae2ffd8fd70ed93cd283b6
fanchao <git@fanchao.dev>
2023-10-23 20:55:05 +1100

    Install.sh script

--- install.sh
+++ install.sh
@@ -3,8 +3,25 @@
 # Exit on error
 set -e
 
+if [[ $(uname -s) != "Linux" ]]; then
+    echo "$0 is only supported on Linux"
+    exit 1
+fi
+
 # Detect CPU arch
 ARCH=$(uname -m)
 
 # Download latest version of the binary from Github
-curl -s https://api.github.com/repos/alexellis/faas/releases/latest \
+echo "Downloading latest version of cron-run for $ARCH..."
+
+if [[ -w /usr/local/bin ]]; then
+    echo "Installing to /usr/local/bin..."
+    curl -fL "https://github.com/simophin/cron-run/releases/download/latest/cron-run.$ARCH" -o /usr/local/bin/cron-run
+    chmod +x /usr/local/bin/cron-run
+else
+    echo "Installing to /usr/local/bin (requires sudo)..."
+    sudo "curl -fL "https://github.com/simophin/cron-run/releases/download/latest/cron-run.$ARCH" -o /usr/local/bin/cron-run && \
+            chmod +x /usr/local/bin/cron-run"
+fi
+
+echo "Done installing cron-run on $ARCH"
